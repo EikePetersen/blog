bad7cd44178656dcd7d524993123f08d1a3dde34
Janis <krishna.kater@gmail.com>
2018-01-26 16:11:18 +0100

    changed the EntryController

---
+++ src/AppBundle/Controller/EntryController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Classes\BlogAsset;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EntryController extends Controller {
+
+    /**
+     * @Route("/blogentry")
+     */
+    public function indexAction() {
+        // The array that holds the type and the url of the asset
+        $assets = array();
+
+        // Fill the asset array
+        $assets[] = new BlogAsset("css", "style.css");
+
+        return $this->render('default/index.html.twig', array(
+            "assets" => $assets,
+        ));
+    }
+}
