a1b18b917d771636e4cfa14ee1c10590b211fba1
Eike <slenderlp8@gmail.com>
2018-01-29 15:58:17 +0100

    Now you can edit the entries (selected by their id)

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -1,9 +1,14 @@
 <?php
 
 namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use AppBundle\Entity\User;
+use AppBundle\Entity\Entry;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 
 class AdminController extends Controller {
@@ -19,14 +24,13 @@ class AdminController extends Controller {
         return $entries;
     }
 
-    public function getEntry($id, $title) {
+    public function getEntry($id) {
 
         $repository = $this->getDoctrine()
             ->getRepository(Entry::class);
 
         $entry = $repository->findOneBy([
             'id' => $id,
-            'title' => $title,
         ]);
 
         if($entry) { $this->error = 0; return $entry; }
@@ -41,18 +45,19 @@ class AdminController extends Controller {
             "content"=>array(
                 "headline"=>"Admin-Berreich",
                 "text"=>"Hier kannst du Beitäge verwalten, löschen und erstellen, User löschen und zum Admin ernennen / zum User degradieren und die statischen Seiten (Über uns & Impressum) bearbeiten.",
+                "entries"=>null,
             ),
         ));
     }
 
     /**
-     * @Route("/admin/entry/", name="edit")
+     * @Route("/admin/entry/", name="entries")
      */
     public function showEntries() {
         return $this->render('default/admin.html.twig', array(
             "content"=>array(
                 "headline"=>"Beiträge",
-                "text"=>"Hier kannst du alle Beiträge bearb",
+                "text"=>"Hier kannst du alle Beiträge bearbeiten.",
             ),
             "entries"=>$this->getEntries(),
         ));
@@ -61,11 +66,49 @@ class AdminController extends Controller {
     /**
      * @Route("/admin/entry/edit/{id}", name="edit")
      */
-    public function editEntry($id) {
-        return $this->render('default/admin.html.twig', array(
+    public function editEntry(Request $request, $id) {
+
+        $entry = $this->getEntry($id);
+
+        $form = $this->createFormBuilder($entry)
+            ->add('title', TextType::class, array('label' => "Titel: ", "required" => true))
+            ->add('content', TextareaType::class, array('label' => "Inhalt: ", "required" => true))
+            ->add('save', SubmitType::class, array('label' => 'Bearbeiten'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $entry = new Entry();
+
+            $entry->setId($form->getData()->getId());
+            $entry->setTitle($form->getData()->getTitle());
+            $entry->setAuthor($form->getData()->getAuthor());
+            $entry->setContent($form->getData()->getContent());
+
+            $preview = preg_replace('%<a href=.*?>(.*)?</a>%', "$1", $form->getData()->getContent());
+            $preview = preg_replace('%<p>(.*)?</p>%', "$1", $preview);
+            if(strlen($preview) > 125) $preview = str_replace($preview, 121) . " ...";
+            else $preview = $preview;
+
+            $entry->setPreview($preview);
+            $entry->setDate($form->getData()->getDate());
+
+            // ... perform some action, such as saving the task to the database
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entry);
+            $em->flush();
+
+            return $this->redirectToRoute('admin');
+        }
+
+        return $this->render('default/edit.html.twig', array(
             "content"=>array(
                 "headline"=>"Beitrag bearbeiten",
                 "text"=>"",
+                'form' => $form->createView(),
             ),
         ));
     }
@@ -78,6 +121,7 @@ class AdminController extends Controller {
             "content"=>array(
                 "headline"=>"Beitrag bearbeiten",
                 "text"=>"",
+                "entries"=>$this->getEntries(),
             ),
         ));
     }
@@ -90,6 +134,7 @@ class AdminController extends Controller {
             "content"=>array(
                 "headline"=>"Beitrag löschen",
                 "text"=>"",
+                "entries"=>null,
             ),
         ));
     }
@@ -102,6 +147,7 @@ class AdminController extends Controller {
             "content"=>array(
                 "headline"=>"Statische Seiten bearbeiten",
                 "text"=>"",
+                "entries"=>null,
             ),
         ));
     }
